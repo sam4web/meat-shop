a91441b886dc9c715ca26d3cb996c54e33e7a3af
Sam <sijal.m06@gmail.com>
2022-11-04 21:24:57 +0545

    header section completed

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ $(document).ready(() => {
 $('.back-to-top').click(() => {
   window.scrollTo(0, 0);
 });
+
+// loads header and footer
+$(document).ready(() => {
+  console.log('ready');
+  $('.header_wrapper').load('/components/header.html');
+  $('.footer_wrapper').load('/components/footer.html');
+});
